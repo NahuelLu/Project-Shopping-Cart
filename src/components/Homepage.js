3eb7a2b94b5658ee40c328e8d3908432ea22cae5
NahuelLu <nahuellucana1999@gmail.com>
2023-08-15 12:06:41 -0300

    Add context to Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,30 +1,21 @@
 import '../styles/Homepage.css'
 import Image from './Image'
-import Img from '../imgs/img.jpg'
-import Img1 from '../imgs/img1.jpg'
-import Img2 from '../imgs/img2.jpg'
-import Img3 from '../imgs/img3.jpg'
-import Img4 from '../imgs/img4.jpg'
-import Img5 from '../imgs/img5.jpg'
 import { useEffect } from 'react'
+import { useShopContext } from './ShopContext'
 
 
 const Homepage = ()=>{
+    const {images} = useShopContext()
     useEffect(()=>{
         return ()=>{console.log("Component was unmounted!")}
     },[]
     )
-    
+
     return(
         <main className="homepage">
             <div><h2>Some random Fan Arts of Michaeng!</h2></div>
             <div className='images-container'>
-                <Image path={Img} ></Image>
-                <Image path={Img1} ></Image>
-                <Image path={Img2} ></Image>
-                <Image path={Img3} ></Image>
-                <Image path={Img4} ></Image>
-                <Image path={Img5} ></Image>
+                {images.map((url,index) => <Image key={index} path={url}></Image>)}
             </div>
         </main>
     )
--- src/components/ShopContext.js
+++ src/components/ShopContext.js
@@ -1,4 +1,10 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import Img from '../imgs/img.jpg'
+import Img1 from '../imgs/img1.jpg'
+import Img2 from '../imgs/img2.jpg'
+import Img3 from '../imgs/img3.jpg'
+import Img4 from '../imgs/img4.jpg'
+import Img5 from '../imgs/img5.jpg'
 
 const ShopContext = createContext();
 
@@ -7,20 +13,27 @@ export function useShopContext() {
 }
 
 export function ShopProvider({ children }) {
-  const [amountItems, setAmountItems] = useState(0);
-  const [books, setBooks] = useState([]);
-  const [itemsCart, setItemsCart] = useState([]);
+    const [amountItems, setAmountItems] = useState(0);
+    const [books, setBooks] = useState([]);
+    const [itemsCart, setItemsCart] = useState([]);
+    const [images, setImages] = useState([])
 
-  const contextValue = {
+    const imagesUrl = [Img,Img1,Img2,Img3,Img4,Img5]
+
+    useEffect(()=>{
+        setImages(imagesUrl)
+    },[])
+    const contextValue = {
     amountItems,
     setAmountItems,
     books,
     setBooks,
     itemsCart,
     setItemsCart,
-  };
+    images
+    };
 
-  return (
+    return (
     <ShopContext.Provider value={contextValue}>{children}</ShopContext.Provider>
-  );
+    );
 }
